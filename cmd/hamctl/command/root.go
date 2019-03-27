e03070eb832119e111333e9b5b448eda54f54d46
Crevil <bjoern.soerensen@gmail.com>
2019-03-27 08:12:36 +0100

    Implemented env variable default value validation for http-auth-token

--- cmd/hamctl/command/root.go
+++ cmd/hamctl/command/root.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -16,6 +17,14 @@ func NewCommand() *cobra.Command {
 		Run: func(c *cobra.Command, args []string) {
 			c.HelpFunc()(c, args)
 		},
+		PersistentPreRunE: func(c *cobra.Command, args []string) error {
+			var err error
+			client.authToken, err = envFlag("http-auth-token", "HAMCTL_AUTH_TOKEN", client.authToken)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 	command.AddCommand(NewPromote(&client))
 	command.AddCommand(NewRelease(&client))
@@ -23,6 +32,17 @@ func NewCommand() *cobra.Command {
 	command.AddCommand(NewPolicy(&client))
 	command.PersistentFlags().DurationVar(&client.timeout, "http-timeout", 20*time.Second, "HTTP request timeout")
 	command.PersistentFlags().StringVar(&client.baseURL, "http-base-url", "https://release-manager.dev.lunarway.com", "address of the http release manager server")
-	command.PersistentFlags().StringVar(&client.authToken, "http-auth-token", os.Getenv("HAMCTL_AUTH_TOKEN"), "auth token for the http service")
+	command.PersistentFlags().StringVar(&client.authToken, "http-auth-token", "", `auth token for the http service (default environment variable "HAMCTL_AUTH_TOKEN")`)
 	return command
+}
+
+func envFlag(flagName, env, flagValue string) (string, error) {
+	envToken := os.Getenv("HAMCTL_AUTH_TOKEN")
+	if flagValue == "" && envToken == "" {
+		return "", errors.New(`required flag "http-auth-token" not set"`)
+	}
+	if flagValue == "" && envToken != "" {
+		return envToken, nil
+	}
+	return flagValue, nil
 }
